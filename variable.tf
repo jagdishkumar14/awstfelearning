aabce7d671f7e2b5e27b732e134e1a5435c8f998
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-09 20:17:25 +0530

    removing description

--- variable.tf
+++ variable.tf
@@ -1,15 +1,13 @@
 variable "aws_ami" {
-  Description = "ami version for ec2 instance"
-  default     = "ami-0c2b8ca1dad447f8a"
+   default     = "ami-0c2b8ca1dad447f8a"
 
 }
 variable "aws_ins_type" {
-  Description = "Aws instance type"
   default     = "t2.micro"
 
 }
 variable "tags" {
-  Description = "Standard tags for learning"
+  Project = "Standard tags for learning"
   Env         = "Learning"
   Account     = "free"
   Type        = "temp"
