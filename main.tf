63b069e90b526d530588f43a3b1d5b7de88f97d8
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-09 20:13:13 +0530

    adding tags variable

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 resource "aws_instance" "web" {
-  ami           = var.aws_ami
-  instance_type = var.aws_ins_type
+  ami               = var.aws_ami
+  instance_type     = var.aws_ins_type
   availability_zone = "us-east-1a"
-  #availability_zone = "us-east-1c"
-
-  tags = {
-    "Name" = "WebApp-terraform"
-    #"Name" = "Nginx"
-  }
+  tags              = var.tags
 }
--- variable.tf
+++ variable.tf
@@ -1,10 +1,16 @@
 variable "aws_ami" {
-    Description= "ami version for ec2 instance"
-    default = "ami-0c2b8ca1dad447f8a"
-  
+  Description = "ami version for ec2 instance"
+  default     = "ami-0c2b8ca1dad447f8a"
+
 }
 variable "aws_ins_type" {
-    Description = "Aws instance type"
-    default = "t2.micro"
-  
+  Description = "Aws instance type"
+  default     = "t2.micro"
+
+}
+variable "tags" {
+  Description = "Standard tags for learning"
+  Env         = "Learning"
+  Account     = "free"
+  Type        = "temp"
 }
