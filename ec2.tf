874139e351905728a58e781ae68ac99d1dfdde5d
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-10 13:03:48 +0530

    updating output file

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami               = var.aws_ami
-  instance_type     = var.aws_ins_type
-  availability_zone = "us-east-1a"
+  ami                    = var.aws_ami
+  instance_type          = var.aws_ins_type
+  availability_zone      = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  tags              = var.tags
+  tags                   = var.tags
 }
 
--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
 output "public_ip" {
   value = aws_instance.web.public_ip
 }
+output "dns_name" {
+  value = aws_instance.web.public_dns
+
+}
+
+output "private_name" {
+  value = aws_instance.web.private_ip
+
+}
