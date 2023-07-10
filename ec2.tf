eb950c49e3acf8ee8c864ea0e8972959332fd0c7
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-10 12:09:48 +0530

    update security group

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami               = var.aws_ami
   instance_type     = var.aws_ins_type
   availability_zone = "us-east-1a"
-  security_groups = aws_security_group.allow_ssh
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags              = var.tags
   network_interface {
     network_interface_id = aws_network_interface.foo.id
