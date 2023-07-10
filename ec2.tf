8f766ac46d639ed31a964e00696f9c7edb85e981
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-10 12:11:28 +0530

    updating sg

--- ec2.tf
+++ ec2.tf
@@ -4,9 +4,5 @@ resource "aws_instance" "web" {
   availability_zone = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags              = var.tags
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
 }
 
