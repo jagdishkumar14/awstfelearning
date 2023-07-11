d08174f3c42c17589f69a2dff32fb442009d8cfa
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 14:20:36 +0530

    removing igw attachment code

--- igw.tf
+++ igw.tf
@@ -3,10 +3,6 @@ resource "aws_internet_gateway" "igw" {
 
   tags = var.tags
 }
-resource "aws_internet_gateway_attachment" "igw_attachment" {
-  vpc_id              = aws_vpc.learning_vpc.id
-  internet_gateway_id = aws_internet_gateway.igw.id
-}
 resource "aws_route_table" "table" {
   vpc_id = aws_vpc.learning_vpc.id
   route {
