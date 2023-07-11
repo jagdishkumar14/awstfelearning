1fa5217f85ef2e431cbebfbd024e31ef1232b724
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 12:06:59 +0530

    update key pair

--- key_pair.tf
+++ key_pair.tf
@@ -15,8 +15,6 @@ resource "aws_key_pair" "this" {
 ################################################################################
 
 resource "tls_private_key" "this" {
-  count = var.create && var.create_private_key ? 1 : 0
-
   algorithm = var.private_key_algorithm
   rsa_bits  = var.private_key_rsa_bits
 }
