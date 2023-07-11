b57ac677e762995fb258c79c01ff4235f7624fd2
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 12:02:09 +0530

    update key module file

--- key_pair.tf
+++ key_pair.tf
@@ -1,14 +1,3 @@
-locals {
-  name   = "ex-${replace(basename(path.cwd), "_", "-")}"
-  region = "eu-west-1"
-
-  tags = {
-    Example    = local.name
-    GithubRepo = "terraform-aws-key-pair"
-    GithubOrg  = "terraform-aws-modules"
-  }
-}
-
 ################################################################################
 # Key Pair Module
 ################################################################################
@@ -16,25 +5,18 @@ locals {
 module "key_pair" {
   source = "../../"
 
-  key_name           = local.name
+  key_name           = ec2_key_pair_new
   create_private_key = true
-
-  tags = local.tags
+  tags = var.tags
 }
 
 module "key_pair_external" {
   source = "../../"
 
-  key_name   = "${local.name}-external"
+  key_name   = "ec2_key_pair"
   public_key = trimspace(tls_private_key.this.public_key_openssh)
 
-  tags = local.tags
-}
-
-module "key_pair_disabled" {
-  source = "../../"
-
-  create = false
+  tags = var.tags
 }
 
 ################################################################################
