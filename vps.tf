daebe82a697b766227a4f910924c4f1870a9157c
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 15:02:41 +0530

    update subnets

--- vps.tf
+++ vps.tf
@@ -3,15 +3,35 @@ resource "aws_vpc" "learning_vpc" {
   enable_dns_hostnames = "true"
   tags                 = var.tags
 }
-
-resource "aws_subnet" "learning_subnet" {
-  vpc_id            = aws_vpc.learning_vpc.id
-  cidr_block        = "192.168.0.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = var.tags
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = "10.0.1.0/24"
+ tags = var.tags
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = "192.168.0.0/24"
+ tags = var.tags
 }
 
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.learning_vpc.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = var.tags
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.learning_vpc.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = var.tags
+}
 resource "aws_network_interface" "foo" {
   subnet_id   = aws_subnet.learning_subnet.id
   private_ips = ["192.168.0.50"]
