7cf5b3e5ce44de642f2ab1230b3492905db109da
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 14:11:56 +0530

    correcting gateway_id

--- igw.tf
+++ igw.tf
@@ -11,6 +11,6 @@ resource "aws_route_table" "table" {
   vpc_id = aws_vpc.learning_vpc.id
   route {
     cidr_block          = "0.0.0.0/0"
-    internet_gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
