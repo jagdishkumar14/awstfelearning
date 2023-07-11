43436a306a17b8204d5dde82adb8f4af4201820c
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 13:56:10 +0530

    adding igw id

--- igw.tf
+++ igw.tf
@@ -4,6 +4,6 @@ resource "aws_internet_gateway" "igw" {
   tags = var.tags
 }
 resource "aws_internet_gateway_attachment" "igw_attachment" {
-    vpc_id = aws_vpc.learning_vpc.id
-  
+  vpc_id              = aws_vpc.learning_vpc.id
+  internet_gateway_id = aws_internet_gateway.igw.id
 }
