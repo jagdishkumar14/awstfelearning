c74491b8c876f9d38ebcb8d149906b4f10b04eb9
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 16:40:13 +0530

    tf fmt

--- igw.tf
+++ igw.tf
@@ -4,12 +4,12 @@ resource "aws_internet_gateway" "igw" {
   tags = var.tags
 }
 resource "aws_route_table" "igw_rt" {
- vpc_id = aws_vpc.learning_vpc.id
- 
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.igw.id
- }
- 
- tags = var.tags
- }
+  vpc_id = aws_vpc.learning_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = var.tags
+}
