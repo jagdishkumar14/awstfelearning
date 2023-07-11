7deece04a72de652a238d9559b1530609089385a
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 11:14:20 +0530

    creating key pair for ec2

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,19 @@
 module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
+  source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "single-instance"
 
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "ec2_key_pair"
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id              = aws_subnet.learning_subnet.id
-  tags                   = var.tags
+  tags = var.tags
+  }
+
+module "ec2_key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name           = "ec2_key_pair"
+  create_private_key = true
 }
