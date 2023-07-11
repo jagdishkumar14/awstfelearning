e219191e931f149a0b5da8c2603e98dd7017239c
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 14:10:31 +0530

    terraform fmt

--- igw.tf
+++ igw.tf
@@ -9,8 +9,8 @@ resource "aws_internet_gateway_attachment" "igw_attachment" {
 }
 resource "aws_route_table" "table" {
   vpc_id = aws_vpc.learning_vpc.id
-route {
-    cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block          = "0.0.0.0/0"
     internet_gateway_id = aws_internet_gateway.igw.id
   }
 }
