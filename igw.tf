eb009c30c00dd7f1b969c1dce958011416e21f13
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 14:10:14 +0530

    igw route adding

--- igw.tf
+++ igw.tf
@@ -7,3 +7,10 @@ resource "aws_internet_gateway_attachment" "igw_attachment" {
   vpc_id              = aws_vpc.learning_vpc.id
   internet_gateway_id = aws_internet_gateway.igw.id
 }
+resource "aws_route_table" "table" {
+  vpc_id = aws_vpc.learning_vpc.id
+route {
+    cidr_block = "0.0.0.0/0"
+    internet_gateway_id = aws_internet_gateway.igw.id
+  }
+}
