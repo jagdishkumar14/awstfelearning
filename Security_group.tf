c8586bd4b10a95488413ebd7b6d5a0ad03ea9dd6
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 13:21:02 +0530

    allo all inbound connection

--- Security_group.tf
+++ Security_group.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "allow_ssh" {
 
   ingress {
     description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
