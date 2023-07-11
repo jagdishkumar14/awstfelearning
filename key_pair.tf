d1729ef053fed987fa13c4e30fce74bae4802c08
jagdish <devops.jagdish.kumar@gmail.com>
2023-07-11 12:06:02 +0530

    update key pair

--- key_pair.tf
+++ key_pair.tf
@@ -3,8 +3,6 @@
 ################################################################################
 
 resource "aws_key_pair" "this" {
-  count = var.create ? 1 : 0
-
   key_name        = var.key_name
   key_name_prefix = var.key_name_prefix
   public_key      = var.create_private_key ? trimspace(tls_private_key.this[0].public_key_openssh) : var.public_key
